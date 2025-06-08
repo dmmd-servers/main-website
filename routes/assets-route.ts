1ade956b878a9a0aebc1a30761fd1bb06de20cda
DmmD GM <dm12332131mdgaming@gmail.com>
2025-06-07 21:03:17 -0400

    made assets throw an error immediately instead of ignoring misses

--- routes/assets-route.ts
+++ routes/assets-route.ts
@@ -3,6 +3,7 @@ import nodeFile from "node:fs/promises";
 import nodePath from "node:path";
 import * as project from "../core/project";
 import AbortFault from "../faults/abort-fault";
+import NoEndpointFault from "../faults/no-endpoint-fault";
 
 // Defines assets route function
 export async function assetsRoute(request: Request, server: Bun.Server): Promise<Response> {
@@ -16,13 +17,13 @@ export async function assetsRoute(request: Request, server: Bun.Server): Promise
         // Resolves asset
         const filepath = nodePath.resolve(project.rootPath, "./assets/", target[1]!);
         const stat = await nodeFile.stat(filepath);
-        if(!stat.isFile()) throw new AbortFault();
+        if(!stat.isFile()) throw new NoEndpointFault();
         const file = Bun.file(filepath);
         return new Response(file);
     }
     catch {
         // Throws fault
-        throw new AbortFault();
+        throw new NoEndpointFault();
     }
 }
 
