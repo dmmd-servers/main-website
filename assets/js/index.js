b04dfeb828c3948aaca1894eb00b7bed2cfc7106
DmmD GM <dm12332131mdgaming@gmail.com>
2025-06-08 14:28:26 -0400

    basic website framework done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,68 @@
-// Prints "Hello, world!"
-console.log("Hello, world!");
+// Handles redirects
+if(location.pathname === "/home") window.location.replace("/");
+
+// Handles spa
+const pages = new Map();
+const main = document.getElementById("main");
+if(main === null) throw new Error("No main field found.");
+window.addEventListener("click", async (event) => {
+    // Checks target
+    const target = event.target;
+    if(!(target instanceof HTMLAnchorElement)) return;
+    if(target.origin !== window.location.origin) return;
+
+    // Checks overrides
+    const ignoreSpaOverride = target.attributes.getNamedItem("data-ignore-spa");
+    if(ignoreSpaOverride !== null && ignoreSpaOverride.value === "true") return;
+
+    // Overrides redirect
+    event.preventDefault();
+    if(target.pathname === location.pathname) return;
+    await render(target.pathname);
+});
+window.addEventListener("popstate", async (event) => {
+    // Overrides redirect
+    main.innerHTML = event.state.page;
+});
+await render(location.pathname);
+async function preload(pathname) {
+    // Checks cache
+    const hit = pages.get(pathname);
+    if(typeof hit !== "undefined") return hit;
+
+    // Requests page
+    const result = await fetch("/assets/html" + pathname + ".html");
+    if(!result.ok) throw new Error("Failed to request page.");    
+    const page = await result.text();
+    pages.set(pathname, page);
+    return page;
+}
+async function render(pathname) {
+    // Requests page
+    let page;
+    try {
+        page = await preload(pathname === "/" ? "/home" : pathname);
+    }
+    catch {
+        page = await preload("/error");
+    }
+
+    // Paints content
+    const context = {
+        page: page
+    };
+    main.innerHTML = page;
+    window.history.pushState(context, "", pathname);
+
+    // Executes scripts
+    const scripts = main.querySelectorAll("script");
+    scripts.forEach((script) => {
+        const clone = document.createElement("script");
+        for(let i = 0; i < script.attributes.length; i++) {
+            const attribute = script.attributes[i];
+            clone.setAttribute(attribute.name, attribute.value);
+        }
+        clone.text = script.text;
+        script.parentNode.replaceChild(clone, script);
+    });
+}
