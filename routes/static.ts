a38649f7e5af578e65c7bb64b5eefa03e3856baf
DmmD GM <dm12332131mdgaming@gmail.com>
2025-06-19 18:03:41 -0400

    force static path to always abort

--- routes/static.ts
+++ routes/static.ts
@@ -9,7 +9,7 @@ export async function route(url: URL, request: Request, server: Bun.Server): Pro
     const pattern = url.pathname.match(/^\/(.*)$/);
     if(pattern === null) throw new faults.RouteAbort();
     const filepath = nodePath.resolve(direct.contents, pattern[1]!);
-    if(!filepath.startsWith(direct.contents)) throw new faults.MissingEndpoint();
+    if(!filepath.startsWith(direct.contents)) throw new faults.RouteAbort();
     const file = Bun.file(filepath);
     if(!(await file.exists())) throw new faults.RouteAbort();
     return new Response(file);
