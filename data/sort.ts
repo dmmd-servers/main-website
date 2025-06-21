f69ad2c7d65e5eb530af97f1c9f66eb1f1a5d7a1
DmmD GM <dm12332131mdgaming@gmail.com>
2025-06-21 17:23:40 -0400

    populated data

--- data/sort.ts
+++ data/sort.ts
@@ -1,33 +1,35 @@
 // Imports
-import type { Anime } from "./types";
+import type { Anime, Game, Tag } from "./types";
 import nodePath from "node:path";
 import chalk from "chalk";
 import audit from "../core/audit";
 import direct from "../core/direct";
 
-// Begins autosort
-audit("autosort", "Beginning autosort...", chalk.blue);
+// Begins sort
+audit("sort", "Beginning sort...", chalk.blue);
 
-// Autosorts tags
+// Sorts tags
 const tagsFile = Bun.file(nodePath.resolve(direct.data, "./tags.json"));
-const tagsData = await tagsFile.json() as Anime[];
+const tagsData = await tagsFile.json() as Tag[];
 tagsData.sort((a, b) => a.id.localeCompare(b.id));
 await tagsFile.write(JSON.stringify(tagsData, null, 4));
-audit("autosort", `Autosorted ${tagsData.length} Tag(s).`, chalk.green);
+audit("sort", `Sorted ${tagsData.length} Tag(s).`, chalk.green);
 
-// Autosorts anime
+// Sorts anime
 const animeFile = Bun.file(nodePath.resolve(direct.data, "./anime.json"));
 const animeData = await animeFile.json() as Anime[];
 animeData.sort((a, b) => a.id.localeCompare(b.id));
+animeData.forEach((workData) => workData.tags.sort((a, b) => a.localeCompare(b)));
 await animeFile.write(JSON.stringify(animeData, null, 4));
-audit("autosort", `Autosorted ${animeData.length} Anime.`, chalk.green);
+audit("sort", `Sorted ${animeData.length} Anime.`, chalk.green);
 
-// Autosorts anime
+// Sorts anime
 const gamesFile = Bun.file(nodePath.resolve(direct.data, "./games.json"));
-const gamesData = await gamesFile.json() as Anime[];
+const gamesData = await gamesFile.json() as Game[];
 gamesData.sort((a, b) => a.id.localeCompare(b.id));
+gamesData.forEach((gameData) => gameData.tags.sort((a, b) => a.localeCompare(b)));
 await gamesFile.write(JSON.stringify(gamesData, null, 4));
-audit("autosort", `Autosorted ${gamesData.length} Game(s).`, chalk.green);
+audit("sort", `Sorted ${gamesData.length} Game(s).`, chalk.green);
 
-// Ends autosort
-audit("autosort", "Ending autosort...", chalk.blue);
+// Ends sort
+audit("sort", "Ending sort...", chalk.blue);
