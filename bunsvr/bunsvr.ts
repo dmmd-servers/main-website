5ea9a0a0822ce2c0bde0dcdefbd448528b5e70f9
DmmD GM <dm12332131mdgaming@gmail.com>
2025-07-11 00:17:55 -0400

    bumped to v1.4.4

--- bunsvr/bunsvr.ts
+++ bunsvr/bunsvr.ts
@@ -6,7 +6,7 @@ export const description = "DmmD's barebone Bun http/https server template.";
 export const license = "MIT";
 export const name = "bunsvr";
 export const repository = "https://github.com/dmmd-stuff/bunsvr";
-export const version = "1.4.4-dev";
+export const version = "1.4.4";
 
 // Exports
 export default {
